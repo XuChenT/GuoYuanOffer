7e32eb89ce8db61c11631e4c4899f77b3d44c153
unknown <313312835@qq.com>
2019-11-15 21:07:50 +0800

    init

---
+++ project1/src/project1/hellowWorld.java
@@ -0,0 +1,10 @@
+package project1;
+
+import java.io.IOException;
+
+public class hellowWorld {
+	public static void main(String[] args){
+	System.out.println("hello World");
+	System.out.println("hello w");
+    }
+}
